720a02d7087fd54b25a844e94fb56a09cd842054
Rob Johnson <rjohnson@appliedsystems.com>
2024-09-11 22:12:23 +0100

    Completed ServiceComposer unit tests

--- test/HandUp.UnitTests/ServiceComposition/ServiceComposerTests.cs
+++ test/HandUp.UnitTests/ServiceComposition/ServiceComposerTests.cs
@@ -1,21 +1,22 @@
 ﻿using HandUp.ServiceComposition;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NSubstitute;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace HandUp.UnitTests.ServiceComposition;
 
 public class ServiceComposerTests
 {
-    private readonly ILogger<ServiceComposer> logger;
+    private readonly TestLogger logger;
     private readonly HandUpConfiguration configuration;
     private readonly TheRequest request;
     private readonly TheResponse response;
-    
+
     public ServiceComposerTests()
     {
-        logger = Substitute.For<ILogger<ServiceComposer>>();
+        logger = new TestLogger();
         configuration = new HandUpConfiguration();
         request = new TheRequest();
         response = new TheResponse();
@@ -26,7 +27,7 @@ public class ServiceComposerTests
     {
         // Arrange
         var serviceCollection = new ServiceCollection();
-        
+
         await using var scopedServiceProvider = serviceCollection.BuildServiceProvider();
 
         var sut = new ServiceComposer(logger, configuration, scopedServiceProvider);
@@ -46,7 +47,7 @@ public class ServiceComposerTests
         // Arrange
         var serviceCollection = new ServiceCollection()
             .AddScoped<RequestParticipator<TheRequest, TheResponse>, InitializerParticipator1>()
-            .AddScoped<RequestParticipator<TheRequest, TheResponse>, InitializerParticipator1>();
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, InitializerParticipator2>();
 
         await using var scopedServiceProvider = serviceCollection.BuildServiceProvider();
 
@@ -88,7 +89,7 @@ public class ServiceComposerTests
     }
 
     [Fact]
-    public async Task ComposeAsync_WhenThereIsASingleStructureInitializer_AndAnotherTwoInitializers_ShouldRunTheInitialParticipatorFirst_ThenTheOthers_AndReturnResult()
+    public async Task ComposeAsync_WhenThereIsASingleStructureInitializerParticipator_AndAnotherTwoParticipators_ShouldRunTheInitialParticipatorFirst_ThenTheOthers_AndReturnResult()
     {
         // Arrange
         var serviceCollection = new ServiceCollection()
@@ -118,6 +119,137 @@ public class ServiceComposerTests
         Assert.Equal(request.Value2ForSubsequentButSetByInitializer, result.Response.Value2ForSubsequentButSetByInitializer);
         Assert.Equal(request.Value2ForSubsequentButSetByInitializer, result.Response.SubsequentValue2);
     }
+
+    [Fact]
+    public async Task ComposeAsync_WhenJustToTwoNonInitializerParticipators_ShouldRunTheParticipators_AndReturnResult()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, NonSubsequentParticipator1>()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, NonSubsequentParticipator2>();
+
+        await using var scopedServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var sut = new ServiceComposer(logger, configuration, scopedServiceProvider);
+
+        request.ValueForParticipator1 = Guid.NewGuid();
+        request.ValueForParticipator2 = Guid.NewGuid();
+
+        // Act
+        var result = await sut.ComposeAsync(request, response);
+
+        // Assert
+        Assert.False(result.HasErrors);
+        Assert.Empty(result.Errors);
+        Assert.False(result.NotFoundOrNoResults);
+        Assert.False(result.StructureInitialized);
+        Assert.Equal(Guid.Empty, result.Response.ValueSetByStructureInitializer);
+        Assert.Equal(Guid.Empty, result.Response.ValueForSubsequentButSetByInitializer);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentValue);
+        Assert.Equal(Guid.Empty, result.Response.Value2ForSubsequentButSetByInitializer);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentValue2);
+        Assert.Equal(request.ValueForParticipator1, result.Response.ValueForParticipator1);
+        Assert.Equal(request.ValueForParticipator2, result.Response.ValueForParticipator2);
+    }
+
+    [Fact]
+    public async Task ComposeAsync_WhenThereIsASingleStructureInitializerParticipatorWhichThrows_AndAnotherTwoParticipators_ShouldRunTheInitialParticipatorFirst_ThenFail_ThenRollbackOnlyTheInitializer()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, InitializerParticipator1>()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, SubsequentParticipator1>()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, SubsequentParticipator2>();
+
+        await using var scopedServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var sut = new ServiceComposer(logger, configuration, scopedServiceProvider);
+
+        request.ValueForStructureInitializer = Guid.NewGuid();
+        request.ValueForSubsequentButSetByInitializer = Guid.NewGuid();
+        request.Value2ForSubsequentButSetByInitializer = Guid.NewGuid();
+        request.InitializerShouldThrow = true;
+        request.InitializerErrorMessage = Guid.NewGuid().ToString();
+        request.InitializerRollbackValue = Guid.NewGuid();
+        request.SubsequentRollbackValue = Guid.NewGuid();
+        request.SubsequentRollbackValue2 = Guid.NewGuid();
+
+        // Act
+        var result = await sut.ComposeAsync(request, response);
+
+        // Assert
+        Assert.True(result.HasErrors);
+        Assert.Single(result.Errors);
+        Assert.Equal("An exception was handled during service participation. See logs", result.Errors.Single());
+        Assert.False(result.NotFoundOrNoResults);
+        Assert.True(result.StructureInitialized);
+        Assert.Equal(request.ValueForStructureInitializer, result.Response.ValueSetByStructureInitializer);
+        Assert.Equal(request.ValueForSubsequentButSetByInitializer, result.Response.ValueForSubsequentButSetByInitializer);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentValue);
+        Assert.Equal(request.Value2ForSubsequentButSetByInitializer, result.Response.Value2ForSubsequentButSetByInitializer);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentValue2);
+        Assert.Equal(request.InitializerRollbackValue, result.Response.InitializerRollbackValue);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentRollbackValue);
+        Assert.Equal(Guid.Empty, result.Response.SubsequentRollbackValue2);
+
+        Assert.True(
+            logger.LoggedException<Exception>(
+                request.InitializerErrorMessage,
+                "An exception was handled during request handling for request {requestType} and response {responseType}. Attempting rollback",
+                typeof(TheRequest), 
+                typeof(TheResponse)
+            )
+        );
+    }
+
+    [Fact]
+    public async Task ComposeAsync_WhenThereIsASingleStructureInitializerParticipatorWhichThrows_AndAnotherTwoParticipators_ShouldRunTheInitialParticipatorFirst_ThenFailRunningTheOthers_ThenRollbackAll()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, InitializerParticipator1>()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, SubsequentParticipator1>()
+            .AddScoped<RequestParticipator<TheRequest, TheResponse>, SubsequentParticipator2>();
+
+        await using var scopedServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var sut = new ServiceComposer(logger, configuration, scopedServiceProvider);
+
+        request.ValueForStructureInitializer = Guid.NewGuid();
+        request.ValueForSubsequentButSetByInitializer = Guid.NewGuid();
+        request.Value2ForSubsequentButSetByInitializer = Guid.NewGuid();
+        request.SubsequentRollbackValue = Guid.NewGuid();
+        request.SubsequentRollbackValue2 = Guid.NewGuid();
+        request.Subsequent2ShouldError = true;
+        request.Subsequent2Error = Guid.NewGuid().ToString();
+
+        // Act
+        var result = await sut.ComposeAsync(request, response);
+
+        // Assert
+        Assert.True(result.HasErrors);
+        Assert.Single(result.Errors);
+        Assert.Equal("An exception was handled during service participation. See logs", result.Errors.Single());
+        Assert.False(result.NotFoundOrNoResults);
+        Assert.True(result.StructureInitialized);
+        Assert.Equal(request.ValueForStructureInitializer, result.Response.ValueSetByStructureInitializer);
+        Assert.Equal(request.ValueForSubsequentButSetByInitializer, result.Response.ValueForSubsequentButSetByInitializer);
+        Assert.Equal(request.ValueForSubsequentButSetByInitializer, result.Response.SubsequentValue);
+        Assert.Equal(request.Value2ForSubsequentButSetByInitializer, result.Response.Value2ForSubsequentButSetByInitializer);
+        Assert.Equal(request.Value2ForSubsequentButSetByInitializer, result.Response.SubsequentValue2);
+        Assert.Equal(request.InitializerRollbackValue, result.Response.InitializerRollbackValue);
+        Assert.Equal(request.SubsequentRollbackValue, result.Response.SubsequentRollbackValue);
+        Assert.Equal(request.SubsequentRollbackValue2, result.Response.SubsequentRollbackValue2);
+
+        Assert.True(
+            logger.LoggedException<Exception>(
+                request.Subsequent2Error,
+                "An exception was handled during request handling for request {requestType} and response {responseType}. Attempting rollback",
+                typeof(TheRequest),
+                typeof(TheResponse)
+            )
+        );
+    }
 }
 
 public record TheRequest
@@ -125,6 +257,15 @@ public record TheRequest
     public Guid ValueForStructureInitializer { get; set; }
     public Guid ValueForSubsequentButSetByInitializer { get; set; }
     public Guid Value2ForSubsequentButSetByInitializer { get; set; }
+    public Guid ValueForParticipator1 { get; set; }
+    public Guid ValueForParticipator2 { get; set; }
+    public bool InitializerShouldThrow { get; set; }
+    public string InitializerErrorMessage { get; set; } = string.Empty;
+    public Guid InitializerRollbackValue { get; set; }
+    public Guid SubsequentRollbackValue { get; set; }
+    public Guid SubsequentRollbackValue2 { get; set; }
+    public bool Subsequent2ShouldError { get; set; }
+    public string Subsequent2Error { get; set; } = string.Empty;
 }
 
 public record TheResponse
@@ -134,6 +275,11 @@ public record TheResponse
     public Guid Value2ForSubsequentButSetByInitializer { get; set; }
     public Guid SubsequentValue { get; set; }
     public Guid SubsequentValue2 { get; set; }
+    public Guid ValueForParticipator1 { get; set; }
+    public Guid ValueForParticipator2 { get; set; }
+    public Guid InitializerRollbackValue { get; set; }
+    public Guid SubsequentRollbackValue { get; set; }
+    public Guid SubsequentRollbackValue2 { get; set; }
 }
 
 public class InitializerParticipator1 : RequestParticipator<TheRequest, TheResponse>
@@ -159,6 +305,18 @@ public class InitializerParticipator1 : RequestParticipator<TheRequest, TheRespo
         ongoingComposeResult.Response.Value2ForSubsequentButSetByInitializer = request.Value2ForSubsequentButSetByInitializer;
 
         ongoingComposeResult.StructureInitialized = true;
+
+        if (request.InitializerShouldThrow)
+        {
+            throw new Exception(request.InitializerErrorMessage);
+        }
+    }
+
+    public override async Task RollbackAsync(TheRequest request, ComposeResult<TheResponse> ongoingComposeResult)
+    {
+        await Task.CompletedTask;
+
+        ongoingComposeResult.Response.InitializerRollbackValue = request.InitializerRollbackValue;
     }
 }
 
@@ -184,6 +342,13 @@ public class SubsequentParticipator1 : RequestParticipator<TheRequest, TheRespon
 
         ongoingComposeResult.Response.SubsequentValue = ongoingComposeResult.Response.ValueForSubsequentButSetByInitializer;
     }
+
+    public override async Task RollbackAsync(TheRequest request, ComposeResult<TheResponse> ongoingComposeResult)
+    {
+        await Task.CompletedTask;
+
+        ongoingComposeResult.Response.SubsequentRollbackValue = request.SubsequentRollbackValue;
+    }
 }
 
 public class SubsequentParticipator2 : RequestParticipator<TheRequest, TheResponse>
@@ -200,5 +365,105 @@ public class SubsequentParticipator2 : RequestParticipator<TheRequest, TheRespon
         }
 
         ongoingComposeResult.Response.SubsequentValue2 = ongoingComposeResult.Response.Value2ForSubsequentButSetByInitializer;
+
+        if (request.Subsequent2ShouldError)
+        {
+            throw new Exception(request.Subsequent2Error);
+        }
+    }
+
+    public override async Task RollbackAsync(TheRequest request, ComposeResult<TheResponse> ongoingComposeResult)
+    {
+        await Task.CompletedTask;
+
+        ongoingComposeResult.Response.SubsequentRollbackValue2 = request.SubsequentRollbackValue2;
+    }
+}
+
+public class NonSubsequentParticipator1 : RequestParticipator<TheRequest, TheResponse>
+{
+    public override async Task ParticipateAsync(TheRequest request, ComposeResult<TheResponse> ongoingComposeResult)
+    {
+        await Task.CompletedTask;
+
+        if (ongoingComposeResult.Response.ValueForParticipator1 != Guid.Empty)
+        {
+            throw new Exception("Values already set");
+        }
+
+        ongoingComposeResult.Response.ValueForParticipator1 = request.ValueForParticipator1;
+    }
+}
+
+public class NonSubsequentParticipator2 : RequestParticipator<TheRequest, TheResponse>
+{
+    public override async Task ParticipateAsync(TheRequest request, ComposeResult<TheResponse> ongoingComposeResult)
+    {
+        await Task.CompletedTask;
+
+        if (ongoingComposeResult.Response.ValueForParticipator2 != Guid.Empty)
+        {
+            throw new Exception("Values already set");
+        }
+
+        ongoingComposeResult.Response.ValueForParticipator2 = request.ValueForParticipator2;
+    }
+}
+
+public class TestLogger : ILogger<ServiceComposer>
+{
+    private readonly List<(LogLevel LogLevel, string FinalMessage, Exception? Exception)> logs = [];
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        var finalMessage = formatter.Invoke(state, exception);
+
+        logs.Add((logLevel, finalMessage, exception));
+    }
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public IDisposable? BeginScope<TState>(TState state)
+        where TState : notnull => null;
+
+    public bool LoggedException<TException>(string exceptionMessage, string template, params object[] args)
+        where TException : Exception
+    {
+        var foundByException = logs.Where(x => 
+            x.Exception != null && 
+            x.Exception.GetType() == typeof(TException) &&
+            x.Exception.Message == exceptionMessage
+        ).ToArray();
+
+        if (foundByException.Length != 1)
+        {
+            return false;
+        }
+
+        var log = foundByException.Single();
+
+        var expectedFinalMessage = FormatLogMessage(template, args);
+
+        return log.FinalMessage == expectedFinalMessage && log.LogLevel == LogLevel.Error;
+    }
+
+    private static string FormatLogMessage(string template, params object[] args)
+    {
+#pragma warning disable SYSLIB1045
+        var regex = new Regex(@"\{(\w+)\}");
+#pragma warning restore SYSLIB1045
+        var matches = regex.Matches(template);
+
+        if (matches.Count != args.Length)
+        {
+            throw new ArgumentException("The number of placeholders does not match the number of arguments.");
+        }
+
+        for (var i = 0; i < matches.Count; i++)
+        {
+            template = template.Replace(matches[i].Value, args[i].ToString());
+        }
+
+        return template;
     }
 }
