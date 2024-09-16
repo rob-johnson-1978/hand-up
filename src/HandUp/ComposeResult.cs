32a17c7fc214525fe33c943d8c5e77ea03856043
Rob Johnson <rjohnson@appliedsystems.com>
2024-09-16 13:47:12 +0100

    Removed TempData. It's a smell.

--- src/HandUp/ComposeResult.cs
+++ src/HandUp/ComposeResult.cs
@@ -6,5 +6,4 @@ public record ComposeResult<TResponse>(TResponse Response)
     public bool StructureInitialized { get; set; }
     public List<string> Errors { get; } = [];
     public bool HasErrors => Errors.Count > 0;
-    public dynamic TempData { get; } = new { };
 }
